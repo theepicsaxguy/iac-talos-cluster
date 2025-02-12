34a3fd81c2669ec6f30236fb9c28150d1c1a7f3d
theepicsaxguy <Benjamin.sanden@hotmail.com>
2025-02-12 12:28:47 +0100

    update main.tf

--- modules/providers/main.tf
+++ modules/providers/main.tf
@@ -13,11 +13,3 @@ provider "helm" {
     cluster_ca_certificate = var.cluster_ca_certificate
   }
 }
-
-provider "kubernetes" {
-  host = var.kubernetes_endpoint
-
-  client_certificate     = var.client_certificate
-  client_key             = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
-}
